4b345a36461290d7b58b02d5c2e2bc37490c488c
Yinger <927994040@qq.com>
2024-04-13 20:23:38 +0800

    20240413 add binary search problem leetcode 704 search location

---
+++ algorithm/src/binary_search/problems/Day_20240413_Search.java
@@ -0,0 +1,26 @@
+package binary_search.problems;
+
+/**
+ * @author hcUltra
+ * @description TODO
+ * @date 2024/4/13 20:20
+ **/
+public class Day_20240413_Search {
+    class Solution {
+        public int search(int[] nums, int target) {
+            int left = 0, right = nums.length - 1, mid = 0;
+            while (left <= right) {
+                mid = left + ((right - left) >> 1);
+                if (nums[mid] < target) {
+                    left = mid + 1;
+                } else if (nums[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    return mid;
+                }
+            }
+            return -1;
+        }
+    }
+
+}
